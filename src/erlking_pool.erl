492d803b77f9e1d1d0790736d332e53aa9dbd931
Norbert Melzer <timmelzer@gmail.com>
2015-05-20 14:51:44 +0200

    .

--- src/erlking_pool.erl
+++ src/erlking_pool.erl
@@ -1,7 +1,5 @@
 -module (erlking_pool). 
 
--behaviour (gen_server).
-
 -export ([add_job/1]).
 
 %-define (STRATEGY, best_worker).
@@ -9,8 +7,7 @@
  -define (STRATEGY, next_worker).
 % -define (STRATEGY, available_worker).
 
-add_job({low, Job}) ->
-  wpool:cast(erlking_low_pool, Job, ?STRATEGY);
+-spec add_job(any()) -> ok.
 add_job(Job) ->
   %io:format("Distributing Job: ~w~n", [Job]),
   %lager:info("~w has ~w messages waiting.~n", [self(), erlang:process_info(self(), messages_queue_len)]),
