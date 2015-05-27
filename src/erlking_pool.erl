b944d4444fc1f3fec3e13ac16727d470da521f92
Norbert Melzer <timmelzer@gmail.com>
2015-05-27 07:14:54 +0200

    distribute jobs over 3 different sized pools

--- src/erlking_pool.erl
+++ src/erlking_pool.erl
@@ -8,7 +8,13 @@
 % -define (STRATEGY, available_worker).
 
 -spec add_job(any()) -> ok.
+add_job({progress, Board, Weight, Move, History}) ->
+    Pool = if Weight >= 7 -> erlking_hi;
+              Weight >= 4 -> erlking_mid;
+              true        -> erlking_low
+           end,
+    wpool:cast(Pool, {progress, Board, Move, History}, ?STRATEGY);
 add_job(Job) ->
   %io:format("Distributing Job: ~w~n", [Job]),
   %lager:info("~w has ~w messages waiting.~n", [self(), erlang:process_info(self(), messages_queue_len)]),
-  wpool:cast(erlking_pool, Job, ?STRATEGY).
+  wpool:cast(erlking_low, Job, ?STRATEGY).
