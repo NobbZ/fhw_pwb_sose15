f6fcc772d1f693763b2fed7d83fca471bf06fd72
Norbert Melzer <timmelzer@gmail.com>
2015-06-01 14:57:47 +0200

    Save Boards in ETS before sending job

--- src/ek_worker.erl
+++ src/ek_worker.erl
@@ -3,7 +3,7 @@
 -export([start_link/1]).
 
 -record(state, {proxy = undefined :: port() | undefined,
-  lastscore = -4294836225 :: integer()}).
+                lastscore = -4294836225 :: integer()}).
 
 -include("job.hrl").
 
@@ -15,16 +15,16 @@ start_link(Proxy) ->
 loop(#state{proxy = Proxy, lastscore = Last} = State) ->
   ek_proxy:ask_for_job(Proxy, self()),
   State1 = receive
-             no_job ->
-               State;
-             Job ->
-               {Jobs, Score, Hist} = process_job(Job),
-               NewLast = tell_score(Score, Hist, Last),
-               lists:map(fun(#job{} = FunJob) ->
-                 ek_pool:add_job(FunJob)
-               end, Jobs),
-               State#state{lastscore = NewLast}
-           end,
+    no_job ->
+      State;
+    Job ->
+      {Jobs, Score, Hist} = process_job(Job),
+      NewLast = tell_score(Score, Hist, Last),
+      lists:map(fun(#job{} = FunJob) ->
+        ek_pool:add_job(FunJob)
+                end, Jobs),
+      State#state{lastscore = NewLast}
+  end,
   loop(State1).
 
 tell_score(Score, History, LastScore) ->
@@ -33,27 +33,30 @@ tell_score(Score, History, LastScore) ->
     false -> LastScore
   end.
 
-process_job(#job{potential = Pot,
-  board = Board,
-  click = Click,
-  history = History,
-  lastscore = Last,
-  whitespace = WS}) ->
+process_job(#job{potential  = Pot,
+                 board      = BoardHash,
+                 click      = Click,
+                 history    = History,
+                 lastscore  = Last,
+                 whitespace = WS}) ->
+  Board = ets:lookup_element(jobstore, BoardHash, 2),
   {NewBoard, JobScore} = ek_gameboard:makemove(Board, Click),
+  NewBoardHash = erlang:phash2(NewBoard),
+  ets:insert(jobstore, {NewBoardHash, NewBoard}),
   InterScore = Last + JobScore,
-  NewHistory = [Click | History],
+  NewHistory = [Click|History],
   Moves = ek_gameboard:find_clickables(NewBoard),
   %% In der Theorie sinkt die Penalty jedesmal um den Betrag des letzten Klicks
   %% EndScore = InterScore + JobScore, %% InterScore + gameboard:endgame(NewBoard),
   %% Aber das funktioniert doch nicht :( Muss ich weiter überlegen wie ich mir das einsparen kann
   EndScore = InterScore + ek_gameboard:endgame(NewBoard),
   Jobs = lists:map(fun({ThisClick, P}) ->
-    #job{potential = P,
-      board = NewBoard,
-      click = ThisClick,
-      history = NewHistory,
-      lastscore = InterScore,
-      %%lastscore = EndScore,
-      whitespace = WS + Pot}
-  end, Moves),
+    #job{potential  = P,
+         board      = NewBoardHash,
+         click      = ThisClick,
+         history    = NewHistory,
+         lastscore  = InterScore,
+         %%lastscore = EndScore,
+         whitespace = WS + Pot}
+     end, Moves),
   {Jobs, EndScore, NewHistory}.
