cf3d16508745c2a261791c94d91895d0b2be3b58
inf100760 <inf100760@ca69dee5-b57f-43a3-a0df-7eff3ae7a8db>
2015-06-10 07:32:43 +0000

    Some typeannotations in heaps.erl
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_11@39 ca69dee5-b57f-43a3-a0df-7eff3ae7a8db

--- src/heaps.erl
+++ src/heaps.erl
@@ -1,13 +1,24 @@
 -module (heaps).
 -export ([new/0, new/1, add/2, drop/1, from_list/1, from_list/2, is_empty/1,
-          fetch/1, peek/1]).
+          fetch/1, peek/1, to_list/1]).
+
+-export_type([compare/0, t/0]).
 
 -ifdef (TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--record (prioq, {heap = nil, size = 0, compare = fun def_compare/2}).
+-type compare() :: fun((any(), any()) -> lt | eq | gt).
 
+-record (prioq, {heap = nil :: nil | tuple(),
+                 size = 0   :: integer(),
+                 compare = fun def_compare/2 :: compare()
+                }
+        ).
+
+-opaque t() :: #prioq{}.
+
+-spec def_compare(any(), any()) -> lt | eq | gt.
 def_compare(E1, E2) ->
   if E1 <  E2 -> lt;
      E1 == E2 -> eq;
@@ -15,37 +26,47 @@ def_compare(E1, E2) ->
   end.
 
 %% @doc Create an empty priorityqueue.
+-spec new() -> t().
 new() -> #prioq{}.
 
 %% @doc Create an empty priorityqueue with a given `Compare'-function.
 %% 
 %% The `Compare'-function needs to return `lt' when `E1' is lesser than `E2`,
 %% `eq' on equality, and `gt' otherwise.
+-spec new(compare()) -> t().
 new(Compare) -> #prioq{compare = Compare}.
 
 %% @doc Adds an element `E' to the priority queue `Q'.
+-spec add(t(), any()) -> t().
 add(_Q = #prioq{heap = Heap, size = N, compare = Compare}, E) ->
   #prioq{heap = meld(Heap, {E, []}, Compare), size = N+1}.
 
+-spec drop(t()) -> t().
 drop(_Q = #prioq{heap = {_, Sub}, size = N, compare = Compare}) ->
   #prioq{heap = pair(Sub, Compare), size = N-1}.
 
+-spec from_list(list()) -> t().
 from_list(L) ->
   lists:foldl(fun(E, Q) -> add(Q, E) end, new(), L).
 
+-spec from_list(list(), compare()) -> t().
 from_list(L, Compare) ->
   lists:foldl(fun(E, Q) -> add(Q, E) end, new(Compare), L).
 
+-spec to_list(t()) -> list().
 to_list(#prioq{heap = nil, size = 0}) -> [];
 to_list(Q) ->
   [peek(Q) | to_list(drop(Q))].
 
+-spec is_empty(t()) -> boolean().
 is_empty(#prioq{heap = nil, size = 0}) -> true;
 is_empty(_) -> false.
 
+-spec fetch(t()) -> {any(), t()}.
 fetch(Q) ->
   {peek(Q), drop(Q)}.
 
+-spec peek(t()) -> any().
 peek(_Q = #prioq{heap = {X, _}}) ->
   X.
 
