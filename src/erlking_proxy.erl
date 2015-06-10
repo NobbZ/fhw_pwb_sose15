f5130fde8f29f4443636cac1dc84366ba52ddebb
inf100760 <inf100760@ca69dee5-b57f-43a3-a0df-7eff3ae7a8db>
2015-06-10 07:32:51 +0000

    Do changeswitch depending on mem-usage
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_11@69 ca69dee5-b57f-43a3-a0df-7eff3ae7a8db

--- src/erlking_proxy.erl
+++ src/erlking_proxy.erl
@@ -2,42 +2,62 @@
 
 -behaviour(gen_fsm).
 
+-include("job.hrl").
+
 %%% gen_fsm
--export([init/1, collecting/2]).
+-export([init/1, collecting/2, dropping/2]).
 
 %%% api
--export([start_link/0]).
+-export([start_link/0,ask_for_job/1]).
 
 -export_types([]).
 
 -record(state, {worker = undefined                :: pid() | undefined,
                 heap   = heaps:new(fun compare/2) :: heaps:t()}).
 
--define(MAX_HEAP_SIZE, 10000).
+-define(MAX_HEAP_SIZE, 500000).
 -define(MIN_HEAP_SIZE, (round(?MAX_HEAP_SIZE * 0.9))).
+
+-define(MAX_MEM_USE, 1024 * 1024 * 1024 * 3).
+-define(MIN_MEM_USE, (round(?MAX_MEM_USE * 0.9))).
 
 ask_for_job(Proxy) ->
     gen_fsm:send_event(Proxy, ask_for_job).
 
-compare(foo, bar) ->
-    undefined.
+compare(#job{potential = P1}, #job{potential = P2}) ->
+    if P1 >  P2 -> lt;
+       true     -> gt
+    end.
 
 start_link() ->
-    gen_fsm:start_link({local, ?MODULE}, ?MODULE, [], []).
+    lager:info("start_link of proxy ~w", [self()]),
+    gen_fsm:start_link(?MODULE, [], []).
 
-%% @todo spawn real worker
 init([]) ->
-    Worker = undefined, %spawn_link(erlking_worker2, start_link, [self()]),
-    State  = #state{worker = Worker},
+    lager:info("init of proxy ~w", [self()]),
+    Worker = spawn_link(erlking_worker2, start_link, [self()]),
+    lager:info("proxy ~w has started worker ~w", [self(), Worker]),
+    State  = #state{worker = Worker, heap = heaps:new(fun compare/2)},
+    lager:info("proxy ~w enters state of collecting with state ~p",
+               [self(), State]),
     {ok, collecting, State}.
 
 collecting(ask_for_job, #state{worker = Worker, heap = Heap} = State) ->
-    {Job, Heap1} = heaps:fetch(Heap),
-    Worker ! Job,
-    {next_state, collecting, State#state{heap = Heap1}};
+    HeapSize = heaps:size(Heap),
+    Heap2 = if HeapSize > 0 ->
+                    {Job, Heap1} = heaps:fetch(Heap),
+                    Worker ! Job,
+                    Heap1;
+               true -> Heap
+            end,
+    {next_state, collecting, State#state{heap = Heap2}};
 collecting({add_job, Job}, #state{heap = Heap} = State) ->
     Heap1 = heaps:add(Heap, Job),
-    NextState = case heaps:size(Heap1) >= ?MAX_HEAP_SIZE of
+    %% NextState = case heaps:size(Heap1) >= ?MAX_HEAP_SIZE of
+    %%                 true  -> dropping;
+    %%                 false -> collecting
+    %%             end,
+    NextState = case erlang:memory(processes_used) >= ?MAX_MEM_USE of
                     true  -> dropping;
                     false -> collecting
                 end,
@@ -46,18 +66,26 @@ collecting({add_job, Job}, #state{heap = Heap} = State) ->
 dropping(ask_for_job, #state{worker = Worker, heap = Heap} = State) ->
     {Job, Heap1} = heaps:fetch(Heap),
     Worker ! Job,
-    NextState = case heaps:size(Heap1) =< ?MIN_HEAP_SIZE of
+    %% NextState = case heaps:size(Heap1) =< ?MIN_HEAP_SIZE of
+    %%                 true  -> collecting;
+    %%                 false -> dropping
+    %%             end,
+    NextState = case erlang:memory(processes_used) =< ?MIN_MEM_USE of
                     true  -> collecting;
                     false -> dropping
                 end,
-        {next_state, NextState, State#state{heap = Heap1}};
+    {next_state, NextState, State#state{heap = Heap1}};
 dropping({add_job, Job}, #state{heap = Heap} = State) ->
     Heap1 = case compare(Job, heaps:peek(Heap)) of
                 lt -> heaps:add(Heap, Job);
                 eq -> heaps:add(Heap, Job);
                 gt -> Heap
             end,
-    NextState = case heaps:size(Heap1) =< ?MIN_HEAP_SIZE of
+    %% NextState = case heaps:size(Heap1) =< ?MIN_HEAP_SIZE of
+    %%                 true  -> collecting;
+    %%                 false -> dropping
+    %%             end,
+    NextState = case erlang:memory(processes_used) =< ?MIN_MEM_USE of
                     true  -> collecting;
                     false -> dropping
                 end,
