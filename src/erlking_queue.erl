35097ad1daa7e9134874c86a0e016e40180d05fa
inf100760 <inf100760@ca69dee5-b57f-43a3-a0df-7eff3ae7a8db>
2015-06-10 07:32:55 +0000

    removed erlking_queue:result/2
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_11@83 ca69dee5-b57f-43a3-a0df-7eff3ae7a8db

--- src/erlking_queue.erl
+++ src/erlking_queue.erl
@@ -2,7 +2,7 @@
 -behaviour(gen_server).
 
 % API
--export([start_link/0, result/2, result2/2]).
+-export([start_link/0, result2/2]).
 
 % Exports for implementing behaviour 'gen_server'.
 -export([code_change/3, handle_call/3, handle_cast/2, handle_info/2, init/1,
@@ -22,10 +22,6 @@
 start_link() ->
   lager:info("Starting ~p", [?MODULE]),
   gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
-
--spec result([term()], non_neg_integer()) -> non_neg_integer().
-result(Moves, Score) ->
-  gen_server:call(?MODULE, {result, Moves, Score}).
 
 -spec result2([term()], non_neg_integer()) -> non_neg_integer().
 result2(History, Score) ->
