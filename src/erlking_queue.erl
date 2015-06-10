cea2c3fea99a0b8e76126f3599bf949f5bdb19a4
inf100760 <inf100760@ca69dee5-b57f-43a3-a0df-7eff3ae7a8db>
2015-06-10 07:32:39 +0000

    gen_server that handles our state
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_11@22 ca69dee5-b57f-43a3-a0df-7eff3ae7a8db

--- src/erlking_queue.erl
+++ src/erlking_queue.erl
@@ -1,47 +1,50 @@
 -module (erlking_queue).
--export ([start/0, init/0, send_result/3, add_job/2, get_job/1]).
+-behaviour (gen_server).
 
-start() ->
-  spawn(erlking_queue, init, []).
+% API
+-export ([start_link/0, get_job/0, add_job/1]).
 
-init() ->
-  register(queue, self()),
-  loop(0, []).
+% Exports for implementing behaviour 'gen_server'.
+-export ([code_change/3,
+          handle_cast/2,
+          handle_call/3,
+          handle_info/2,
+          init/1]).
 
-loop(MaxScore, JobQueue) ->
-  receive
-    {result, Moves, Score} when Score > MaxScore ->
-      emit_moves_to_stdout(Moves),
-      loop(Score, JobQueue);
-    {result, _Moves, _Score} ->
-      loop(MaxScore, JobQueue);
-    {add_job, Job} ->
-      loop(MaxScore, [Job|JobQueue]);
-    {get_job, Pid} ->
-      JobAvailable = JobQueue /= [],
-      if
-        JobAvailable ->
-          Answer = {ok, lists:nth(1, JobQueue)},
-          NewQueue = lists:nthtail(1, JobQueue);
-        true ->
-          Answer = no_job_available,
-          NewQueue = JobQueue
-      end,
-      Pid ! Answer,
-      loop(MaxScore, NewQueue)
+start_link() ->
+  gen_server:start_link({global, ek_queue}, erlking_queue, [], []).
+
+get_job() ->
+  gen_server:call(ek_queue, get_job).
+
+add_job(Job) ->
+  gen_server:cast(ek_queue, {add_job, Job}).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+init(_) ->
+  {ok, {0, heaps:new()}, infinity}.
+
+code_change(_, _, _) ->
+  {error, "No updates supported!"}.
+
+handle_call(get_job, _From, {MaxScore, JobQueue}) ->
+  IsAvailable = not heaps:is_empty(JobQueue),
+  if IsAvailable -> {Job, NewQueue} = heaps:fetch(JobQueue),
+                    {reply, Job, {MaxScore, NewQueue}};
+     true -> {reply, no_job, {MaxScore, JobQueue}}
   end.
+  
+handle_cast({result, Moves, Score}, {MaxScore, JobQueue}) when Score > MaxScore ->
+  emit_moves_to_stdout(Moves),
+  {noreply, {Score, JobQueue}};
+handle_cast({result, _, _}, State) -> {noreply, State};
+handle_cast({add_job, Job}, {MaxScore, JobQueue}) -> 
+  io:format("Added job: ~w~n", [Job]),
+  {noreply, {MaxScore, heaps:add(JobQueue, Job)}}.
 
-send_result(Pid, Moves, Score) ->
-  Pid ! {result, Moves, Score}.
-
-add_job(Pid, Job) ->
-  Pid ! {add_job, Job}.
-
-get_job(Pid) ->
-  Pid ! {get_job, self()},
-  receive
-    Answer -> Answer
-  end.
+handle_info(_, State) -> {noreply, State}.
 
 emit_moves_to_stdout(Moves) ->
   io:format("[", []),
