a59925f5c6c2568a1893a002fa9b074135fc06f3
inf100760 <inf100760@ca69dee5-b57f-43a3-a0df-7eff3ae7a8db>
2015-06-10 07:33:01 +0000

    Fresh start
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_11@103 ca69dee5-b57f-43a3-a0df-7eff3ae7a8db

--- src/erlking_app.erl
+++ src/erlking_app.erl
@@ -2,8 +2,6 @@
 
 -behaviour(application).
 
--include("job.hrl").
-
 %% Application callbacks
 -export([start/2, stop/1]).
 
@@ -11,26 +9,9 @@
 %% Application callbacks
 %% ===================================================================
 
--spec start(StartType, StartArgs) -> Result when
-  StartType :: applicatio:start_type(),
-  StartArgs :: term(),
-  Result :: {ok, pid()}
-  | {ok, pid(), State}
-  | {error, Reason},
-  Reason :: term(),
-  State :: term().
 start(normal, _StartArgs) ->
   Cores = number_of_cores(),
-  ets:new(jobstore, [set, public, named_table,
-    %% compressed,
-    {keypos, 1}]),
-  ets:new(colorstore, [set, public, {keypos, 1}, named_table]),
-  wpool:start_sup_pool(erlking_test, [{worker, {ek_pworker, []}},
-    {workers, 1 * Cores}]),
   ek_supervisor:start_link(),
-  timer:sleep(100),
-  read_board_from_stdin_and_send_it_as_job(),
-  ek_proxy:run(),
   {ok, self()}.
 
 number_of_cores() ->
@@ -40,48 +21,13 @@ number_of_cores() ->
     C -> C
   end.
 
--spec stop(term()) -> ok.
 stop(_) ->
   ok.
 
-%read_board_from_stdin_and_send_it_as_job() ->
-%  BoardString = io:get_line(""),
-%  BoardMtrx = ek_gameboard:parse_board(BoardString),
-%  ek_sml_lrg_first:run_board(BoardMtrx, [], none, 0).
-
-read_board_from_stdin_and_send_it_as_job() ->
+read_board_from_stdin() ->
   BoardString = io:get_line(""),
-  BoardMtrx = ek_gameboard:parse_board(BoardString),
-  BoardHash = erlang:now(),
-  ets:insert(jobstore, {BoardHash, BoardMtrx}),
-  Moves = ek_gameboard:find_clickables(BoardMtrx),
-  Whitespace = countwhite(BoardMtrx),
-  ColorPercentages = get_color_percentages(BoardMtrx),
-  ets:insert(colorstore, ColorPercentages),
-  Jobs = lists:map(fun({ThisClick, Pot}) ->
-    #job{potential = Pot,
-      board = BoardHash,
-      click = ThisClick,
-      %%lastscore  = Penalty,
-      whitespace = Whitespace}
-  end, Moves),
-  lists:map(fun(Job) ->
-    ek_pool:add_job(Job)
-  end, Jobs).
+  ek_gameboard:parse_board(BoardString).
 
-countwhite(Board) ->
-  Vs = matrix:to_row_vecs(Board),
-  V = lists:foldl(fun(Vec, Acc) -> vector:concat(Acc, Vec) end, vector:from_binary(<<>>), Vs),
-  List = binary_to_list(vector:to_binary(V)),
-  Zeros = lists:filter(fun(E) -> E == 0 end, List),
-  length(Zeros).
 
-get_color_percentages(Board) ->
-  Vs = matrix:to_row_vecs(Board),
-  V = lists:foldl(fun(Vec, Acc) -> vector:concat(Acc, Vec) end, vector:from_binary(<<>>), Vs),
-  List = binary_to_list(vector:to_binary(V)),
-  Stones = lists:foldl(fun(E, Store) -> ek_gameboard:inc_key(E, Store) end, [], List),
-  Stones1 = lists:keydelete(0, 1, Stones),
-  Size = matrix:get_height(Board) * matrix:get_width(Board),
-  lists:keymap(fun(Val) -> Val / Size end, 2, Stones1).
+
 
