f7612b0351acf35736df79816160c28ef196977d
inf100760 <inf100760@ca69dee5-b57f-43a3-a0df-7eff3ae7a8db>
2015-06-10 07:32:37 +0000

    More functions on matrices
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_11@14 ca69dee5-b57f-43a3-a0df-7eff3ae7a8db

--- src/matrix.erl
+++ src/matrix.erl
@@ -4,15 +4,60 @@
 %% Wikipedia article about matrixes</a> for further information.
 -module (matrix).
 
--export ([transpose/1, get_line_vector/2, get_column_vector/2]).
+-export ([transpose/1, get_line_vector/2, get_column_vector/2, num_rows/1,
+          num_cols/1, matrix/3]).
 -export_type ([t/0, t/1]).
 
 -ifdef (TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--type t()    :: t(any()).
--type t(A)   :: [[A]].
+-type t()         :: t(any()).
+-type t(A)        :: [[A]].
+-type generator() :: fun((non_neg_integer(), non_neg_integer()) -> any()).
+
+% ==============================================================================
+% Retrieve information about the matrix
+% ==============================================================================
+
+%% @doc Retrieves the number of rows in a given matrix.
+-spec num_rows(t()) -> pos_integer().
+num_rows(M) ->
+  lists:foldl(fun(_, Len) -> 1 + Len end, 0, M).
+
+%% @doc Retrieves the mumber of columns in a given matrix.
+-spec num_cols(t()) -> pos_integer().
+num_cols(M) ->
+  lists:max(lists:map(fun(E) ->
+    lists:foldl(fun(_, Len) ->
+      1 + Len
+    end, 0, E)
+  end, M)).
+
+% ==============================================================================
+% Builders
+% ==============================================================================
+
+%% @doc Generate a matrix from a generator function.
+-spec matrix(pos_integer(), pos_integer(), generator()) -> t().
+matrix(Rows, Cols, GenFun) ->
+  A = lists:sort(lists:flatten(lists:duplicate(Cols, lists:seq(0, Rows - 1)))),
+  B = lists:flatten(lists:duplicate(Rows, lists:seq(0, Cols - 1))),
+  C = lists:zipwith(GenFun, A, B),
+  splice_list(C, Cols).
+
+-spec splice_list([any()], pos_integer()) -> t().
+splice_list(List, Cols) -> lists:reverse(splice_list(List, Cols, [])).
+
+-spec splice_list([any()], pos_integer, [[any()]]) -> [[any()]].
+splice_list([], _Cols, Acc) -> Acc;
+splice_list(List, Cols, Acc) ->
+  {Vec, Rest} = lists:split(Cols, List),
+  splice_list(Rest, Cols, [Vec|Acc]).
+
+% ==============================================================================
+% Simple transformations
+% ==============================================================================
 
 %% @doc Transposes a matrix.
 %% Transposing a matrix means to reflect the matrix <math><mtext>M</mtext></math>
@@ -33,6 +78,10 @@ transpose([[]|_]) -> [];
 transpose(M) ->
   [lists:map(fun hd/1, M) | transpose(lists:map(fun tl/1, M))].
 
+% ==============================================================================
+% Retrieve subvectors
+% ==============================================================================
+
 %% @doc Gets the specified line-vector.
 %% Counting starts with 0.
 -spec get_line_vector(t(), non_neg_integer()) -> vector:t().
@@ -50,6 +99,17 @@ get_column_vector(Matrix, Column) ->
 % ==============================================================================
 
 -ifdef (TEST).
+
+num_rows_test() ->
+  ?assertEqual(num_rows([[1,2,3],[4,5,6],[7,8,9]]), 3),
+  ?assertEqual(num_rows([[1,2,3]]), 1).
+
+num_cols_test() ->
+  ?assertEqual(num_cols([[1,2,3],[4,5,6],[7,8,9]]), 3),
+  ?assertEqual(num_cols([[1,2,3]]), 3).
+
+matrix_test() ->
+  ?assertEqual(matrix(3, 3, fun(X,Y) -> (X+1) * (Y+1) end), [[1,2,3],[2,4,6],[3,6,9]]).
 
 transpose_1_x_m_test() ->
   ?assertEqual(transpose([[1,2,3]]), [[1],[2],[3]]).
