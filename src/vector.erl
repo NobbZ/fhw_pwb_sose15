d743195f80cd8ef0fef69fe382080eddc6e26b9d
inf100760 <inf100760@ca69dee5-b57f-43a3-a0df-7eff3ae7a8db>
2015-06-10 07:32:53 +0000

    reformatting
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_11@75 ca69dee5-b57f-43a3-a0df-7eff3ae7a8db

--- src/vector.erl
+++ src/vector.erl
@@ -1,22 +1,22 @@
--module (vector).
+-module(vector).
 
--export ([from_binary/1, partition/2, concat/2, get_size/1, to_binary/1]).
--export_type ([t/0]).
+-export([from_binary/1, partition/2, concat/2, get_size/1, to_binary/1]).
+-export_type([t/0]).
 
 -ifdef (TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
--type t()    :: binary() | array:array(any()).
+-type t() :: binary() | array:array(any()).
 -type pred() :: fun((any()) -> boolean()).
 
--record (vector, {size    = 0    :: integer(),
-                  payload = <<>> :: binary() | array:array(any())}).
+-record(vector, {size = 0 :: integer(),
+  payload = <<>> :: binary() | array:array(any())}).
 
 -spec from_binary(binary()) -> t().
 from_binary(<<V/binary>>) ->
-  #vector{size    = byte_size(V),
-          payload = V}.
+  #vector{size = byte_size(V),
+    payload = V}.
 
 -spec to_binary(t()) -> binary().
 to_binary(V) -> V#vector.payload.
@@ -38,31 +38,31 @@ partition_rec(_, <<>>, Acc) -> Acc;
 partition_rec(P, <<E>>, {True, False}) ->
   case P(E) of
     true ->
-      {<<True/binary, <<E>>/binary >>, False};
+      {<<True/binary, <<E>>/binary>>, False};
     false ->
       {True, <<False/binary, <<E>>/binary>>}
   end;
 partition_rec(P, <<E, Es/binary>>, {True, False}) ->
   case P(E) of
     true ->
-      partition_rec(P, Es, {<<True/binary, <<E>>/binary >>, False});
+      partition_rec(P, Es, {<<True/binary, <<E>>/binary>>, False});
     false ->
       partition_rec(P, Es, {True, <<False/binary, <<E>>/binary>>})
   end.
 
 -spec concat(t(), t()) -> t().
 concat(#vector{size = S1, payload = <<V1/binary>>},
-       #vector{size = S2, payload = <<V2/binary>>}) ->
+    #vector{size = S2, payload = <<V2/binary>>}) ->
   #vector{size = S1 + S2, payload = <<V1/binary, V2/binary>>}.
 
 -ifdef (TEST).
 
 partition_test() ->
-  V = from_binary(<<1,2,3,0,0>>),
+  V = from_binary(<<1, 2, 3, 0, 0>>),
   P = fun(E) ->
     E == 0
   end,
-  ?assertEqual({from_binary(<<0,0>>), from_binary(<<1,2,3>>)},
-                partition(P, V)).
+  ?assertEqual({from_binary(<<0, 0>>), from_binary(<<1, 2, 3>>)},
+    partition(P, V)).
 
 -endif.
