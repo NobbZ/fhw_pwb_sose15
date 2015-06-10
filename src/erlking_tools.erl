3c5e3d083ce8fa6bbee4b7c9129e5f59e4cddb6f
inf100760 <inf100760@ca69dee5-b57f-43a3-a0df-7eff3ae7a8db>
2015-06-10 07:32:35 +0000

    .
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_11@6 ca69dee5-b57f-43a3-a0df-7eff3ae7a8db

--- src/erlking_tools.erl
+++ src/erlking_tools.erl
@@ -4,6 +4,16 @@
 
 -export ([parse_board/1, transpose/1]).
 
+-ifdef (TEST).
+-include_lib("eunit/include/eunit.hrl").
+-endif.
+
+-type gameboard() :: matrix(byte()).
+-type matrix()    :: matrix(any()).
+-type matrix(A)   :: [[A]].
+
+%% @doc Parse a given string and return a board
+-spec parse_board(string()) -> gameboard().
 parse_board(Board) ->
   Board_Expr        = string:concat(string:strip(Board, both), "."),
   {ok, Tokens, _}   = erl_scan:string(Board_Expr),
@@ -11,6 +21,37 @@ parse_board(Board) ->
   {value, Value, _} = erl_eval:expr(Form, []),
   transpose(Value).
 
+-spec transpose(matrix()) -> matrix().
 transpose([[]|_]) -> [];
 transpose(M) ->
   [lists:map(fun hd/1, M) | transpose(lists:map(fun tl/1, M))].
+
+
+% ==============================================================================
+% Tests
+% ==============================================================================
+
+-ifdef(TEST).
+
+parse_board_test() ->
+  ?assert(parse_board("[[1,2,3]]") == [[1],[2],[3]]),
+  ?assert(parse_board("[[1,2,3],[4,5,6],[7,8,9]]") == [[1,4,7],[2,5,8],[3,6,9]]).
+
+parse_board_with_whitespace_test() ->
+  ?assert(parse_board("[[1, 2, 3]]") == [[1],[2],[3]]),
+  ?assert(parse_board("[[1, 2, 3], [4, 5, 6], [7, 8, 9]]") == [[1,4,7],[2,5,8],[3,6,9]]).
+
+transpose_1_x_m_test() ->
+  ?assert(transpose([[1,2,3]]) == [[1],[2],[3]]).
+
+transpose_n_x_1_test() ->
+  ?assert(transpose([[1],[2],[3]]) == [[1,2,3]]).
+
+transpose_n_x_m_test() ->
+  ?assert(transpose([[1,2,3],[4,5,6],[7,8,9]]) == [[1,4,7],[2,5,8],[3,6,9]]).
+
+transpose_transpose_is_id_test() ->
+  M = [[1,4,7],[2,5,8],[3,6,9]],
+  ?assert(transpose(transpose(M)) == M).
+
+-endif.
