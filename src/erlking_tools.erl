d289406c18b598906c20aabf0fb75c15f3695665
inf100760 <inf100760@ca69dee5-b57f-43a3-a0df-7eff3ae7a8db>
2015-06-10 07:32:57 +0000

    removed erlking_tools
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_11@87 ca69dee5-b57f-43a3-a0df-7eff3ae7a8db

--- src/erlking_tools.erl
+++
@@ -1,8 +0,0 @@
-%% @doc Some tools needed for Erlking
-%% @author Norbert Melzer <inf100760@fh-wedel.de>
--module(erlking_tools).
-
--ifdef (TEST).
--include_lib("eunit/include/eunit.hrl").
--endif.
-
