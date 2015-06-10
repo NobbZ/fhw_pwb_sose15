7229e20b8265fa77f3dd12bbb7ca632a7d0421c0
inf100760 <inf100760@ca69dee5-b57f-43a3-a0df-7eff3ae7a8db>
2015-06-10 07:33:02 +0000

    extracted drop and take from genome to a list_helper module
    git-svn-id: https://stud.fh-wedel.de/repos/pwb/pwb_11@110 ca69dee5-b57f-43a3-a0df-7eff3ae7a8db

--- src/ek_genome.erl
+++ src/ek_genome.erl
@@ -12,14 +12,6 @@
 %% API
 -export([compare/3, equals/3, splice/3]).
 
-take(_, 0)     -> [];
-take([], _)    -> [];
-take([H|T], N) -> [H|take(T, N - 1)].
-
-drop(L, 0)     -> L;
-drop([], _)    -> [];
-drop([_|T], N) -> drop(T, N - 1).
-
 get_first_normalized([])               -> 0;
 get_first_normalized([0|T])            -> get_first_normalized(T);
 get_first_normalized([X|_]) when X < 0 -> -1;
@@ -27,19 +19,19 @@ get_first_normalized([X|_]) when X > 0 -> 1.
 
 compare(G1, G2, MaxIdx) ->
   F = fun(X, Y) -> X - Y end,
-  L1 = take(G1, MaxIdx),
-  L2 = take(G2, MaxIdx),
+  L1 = ek_list_helpers:take(G1, MaxIdx),
+  L2 = ek_list_helpers:take(G2, MaxIdx),
   L3 = lists:zipwith(F, L1, L2),
   get_first_normalized(L3).
 
 equals(G1, G2, MaxIdx) ->
-  L1 = take(G1, MaxIdx),
-  L2 = take(G2, MaxIdx),
+  L1 = ek_list_helpers:take(G1, MaxIdx),
+  L2 = ek_list_helpers:take(G2, MaxIdx),
   L1 == L2.
 
 splice(_, G2, 0) -> G2;
 splice(G1, G2, SpliceIdx) ->
-  G1a = take(G1, SpliceIdx),
-  G2a = drop(G2, SpliceIdx),
+  G1a = ek_list_helpers:take(G1, SpliceIdx),
+  G2a = ek_list_helpers:drop(G2, SpliceIdx),
   G1a ++ G2a.
 
